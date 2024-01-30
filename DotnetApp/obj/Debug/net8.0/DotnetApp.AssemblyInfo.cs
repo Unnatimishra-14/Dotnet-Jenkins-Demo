96fff275e0f7c7c02c054b11ed328467fe2fa645
Unnati Mishra <unnatim521@gmail.com>
2024-01-30 15:03:33 +0530

    updated file

--- DotnetApp/obj/Debug/net8.0/DotnetApp.AssemblyInfo.cs
+++ DotnetApp/obj/Debug/net8.0/DotnetApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b231892c749c8bba1d3b1f608191e70be5a0bd81")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
